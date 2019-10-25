2ae84adaf29c13cdf40bb084554922e0ca94eb30
king <86416817@qq.com>
2019-10-25 14:16:39 +0800

    cleanup code

--- server-gateway/src/main/java/com/thatday/gateway/config/HystrixFallbackConfiguration.java
+++ server-gateway/src/main/java/com/thatday/gateway/config/HystrixFallbackConfiguration.java
@@ -1,10 +1,10 @@
 package com.thatday.gateway.config;
 
 
+import com.thatday.gateway.provider.ResponseProvider;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -12,11 +12,7 @@ public class HystrixFallbackConfiguration {
 
     @RequestMapping(value = "/globalFallback")
     public Map<String, Object> fallBackController() {
-        Map<String, Object> res = new HashMap();
-        res.put("code", 666666);
-        res.put("data", null);
-        res.put("message", "这里更新中，请稍候。");
-        return res;
+        return ResponseProvider.responseMap(666666, "this services is upgrade");
     }
 
 }
--- server-gateway/src/main/java/com/thatday/gateway/provider/ResponseProvider.java
+++ server-gateway/src/main/java/com/thatday/gateway/provider/ResponseProvider.java
@@ -43,19 +43,22 @@ public class ResponseProvider {
         exchange.getResponse().getHeaders().add("Content-Type", "application/json;charset=UTF-8");
         String result = "";
 
-        Map<String, Object> map = new HashMap<>(16);
-        map.put("code", code);
-        map.put("message", message);
-        map.put("data", null);
-
         try {
-            result = objectMapper.writeValueAsString(map);
+            result = objectMapper.writeValueAsString(responseMap(code, message));
         } catch (JsonProcessingException ignored) {
         }
 
         byte[] bytes = result.getBytes(StandardCharsets.UTF_8);
         DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
         return exchange.getResponse().writeWith(Flux.just(buffer));
+    }
+
+    public static Map<String, Object> responseMap(int code, String message) {
+        Map<String, Object> map = new HashMap<>(16);
+        map.put("code", code);
+        map.put("message", message);
+        map.put("data", null);
+        return map;
     }
 
 }
