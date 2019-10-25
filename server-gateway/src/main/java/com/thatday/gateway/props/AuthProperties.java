cb4572c19224fd9e7d9100ccd92ecad6f4355545
King <86416817@qq.com>
2019-10-25 14:34:44 +0800

    Update AuthProperties.java

--- server-gateway/src/main/java/com/thatday/gateway/props/AuthProperties.java
+++ server-gateway/src/main/java/com/thatday/gateway/props/AuthProperties.java
@@ -1,23 +1,8 @@
-/*
- *      Copyright (c) 2018-2028, Chill Zhuang All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *  Redistributions of source code must retain the above copyright notice,
- *  this list of conditions and the following disclaimer.
- *  Redistributions in binary form must reproduce the above copyright
- *  notice, this list of conditions and the following disclaimer in the
- *  documentation and/or other materials provided with the distribution.
- *  Neither the name of the dreamlu.net developer nor the names of its
- *  contributors may be used to endorse or promote products derived from
- *  this software without specific prior written permission.
- *  Author: Chill 庄骞 (smallchill@163.com)
- */
 package com.thatday.gateway.props;
 
 import lombok.Data;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +11,7 @@ import java.util.List;
  * 权限过滤
  */
 @Data
+@Component
 @RefreshScope
 public class AuthProperties {
 
