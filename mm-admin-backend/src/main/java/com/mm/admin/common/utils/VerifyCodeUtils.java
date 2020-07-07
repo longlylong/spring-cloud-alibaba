70ceb949586934a40b39d14b0d7a90869bf2ba62
King <king@51tcf.com>
2020-07-07 10:02:56 +0800

    验证码改为数字的

--- mm-admin-backend/src/main/java/com/mm/admin/common/utils/VerifyCodeUtils.java
+++ mm-admin-backend/src/main/java/com/mm/admin/common/utils/VerifyCodeUtils.java
@@ -17,7 +17,8 @@ import java.util.Random;
  */
 public class VerifyCodeUtils {
     // 使用到Algerian字体，系统里没有的话需要安装字体，字体只显示大写，去掉了1,0,i,o几个容易混淆的字符
-    public static final String VERIFY_CODES = "123456789ABCDEFGHJKLMNPQRSTUVWXYZ";
+//    public static final String VERIFY_CODES = "123456789ABCDEFGHJKLMNPQRSTUVWXYZ";
+    public static final String VERIFY_CODES = "1234567890";
 
     private static Random random = new SecureRandom();
 
