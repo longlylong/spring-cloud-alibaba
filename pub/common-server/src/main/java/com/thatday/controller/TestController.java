1ca94703bb7ac1ec4cbbb18468bae98bda4d96b7
King <king@51tcf.com>
2020-07-09 13:02:31 +0800

    环境配置

--- pub/common-server/src/main/java/com/thatday/controller/TestController.java
+++ pub/common-server/src/main/java/com/thatday/controller/TestController.java
@@ -7,18 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-
-    @Value("${batch.size.max}")
-    private Integer batchSizeMax;
-
     @Value("${server.port}")
     private int port;
-
 
     @GetMapping("/test")
     public String test() {
         return port + "-test-" + System.currentTimeMillis();
     }
-
 
 }
