20782fe9e9fb5e8f22695cbbb7197875d5c3311e
King <king@51tcf.com>
2020-11-10 11:25:54 +0800

    date utils

--- common/src/main/java/com/thatday/common/utils/DateUtils.java
+++ common/src/main/java/com/thatday/common/utils/DateUtils.java
@@ -4,9 +4,6 @@
 package com.thatday.common.utils;
 
 import cn.hutool.core.date.DateUtil;
-import org.apache.commons.lang3.time.DateFormatUtils;
-
-import java.util.Date;
 
 /**
  * 日期工具类, 继承org.apache.commons.lang.time.DateUtils类
@@ -14,24 +11,13 @@ import java.util.Date;
 public class DateUtils extends DateUtil {
 
 	public static final long MILLIS_PER_DAY = org.apache.commons.lang3.time.DateUtils.MILLIS_PER_DAY;
+	public static final long MILLIS_PER_MINUTE = org.apache.commons.lang3.time.DateUtils.MILLIS_PER_MINUTE;
+	public static final long MILLIS_PER_SECOND = org.apache.commons.lang3.time.DateUtils.MILLIS_PER_SECOND;
 
-	public static String[] parsePatterns = {
+	public static String[] patterns = {
 			"yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM",
 			"yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy/MM",
 			"yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm", "yyyy.MM"};
-
-	/**
-	 * 得到日期字符串 默认格式（yyyy-MM-dd） pattern可以为："yyyy-MM-dd" "HH:mm:ss" "E"
-	 */
-	public static String formatDate(Date date, Object... pattern) {
-		String formatDate = null;
-		if (pattern != null && pattern.length > 0) {
-			formatDate = DateFormatUtils.format(date, pattern[0].toString());
-		} else {
-			formatDate = DateFormatUtils.format(date, "yyyy-MM-dd");
-		}
-		return formatDate;
-	}
 
 
 }
