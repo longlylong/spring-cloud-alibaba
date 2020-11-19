20d18bff53ead2b3859458f89bdb32e0eec1e12f
King <king@51tcf.com>
2020-11-19 10:37:46 +0800

    dubbo配置

--- pub/common-server/src/main/java/com/thatday/service/dubbo/CommonServiceImpl.java
+++ pub/common-server/src/main/java/com/thatday/service/dubbo/CommonServiceImpl.java
@@ -1,14 +1,19 @@
 package com.thatday.service.dubbo;
 
 import com.thatday.common.dubbo.CommonService;
+import com.thatday.config.EnvConfig;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @DubboService
 public class CommonServiceImpl implements CommonService {
 
+    @Autowired
+    EnvConfig envConfig;
+
     @Override
     public String test() {
 //        ThreadUtil.safeSleep(1000);
-        return "dubbo test";
+        return "dubbo test > service post: " + envConfig.getPort();
     }
 }
