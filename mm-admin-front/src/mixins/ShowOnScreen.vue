719032fb1c2013bf0771e5ee3de97a4c3b17af81
King <86416817@qq.com>
2021-07-23 14:33:34 +0800

    util

---
+++ mm-admin-front/src/mixins/ShowOnScreen.vue
@@ -0,0 +1,27 @@
+<template></template>
+
+<script>
+export default {
+  mounted() {
+    const that = this;
+    window.onresize = () => {
+      return () => {
+        window.screenWidth = window.innerWidth;
+        that.screenWidth = window.screenWidth;
+      };
+    };
+  },
+
+  watch: {
+    screenWidth(val) {
+      this.screenWidth = val;
+      this.showOnScreen = this.screenWidth >= 1919;
+    },
+  },
+
+  data: () => ({
+    screenWidth: window.innerWidth,
+    showOnScreen: window.innerWidth >= 1919,
+  }),
+};
+</script>
