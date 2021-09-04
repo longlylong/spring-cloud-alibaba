36174a4d2c80f31f525501744cd934bacad7d62f
King <86416817@qq.com>
2021-09-04 20:16:46 +0800

    base

--- base-service/src/main/java/com/thatday/base/service/BaseService.java
+++ base-service/src/main/java/com/thatday/base/service/BaseService.java
@@ -1,76 +1,9 @@
 package com.thatday.base.service;
 
-import com.thatday.base.SpecificationListener;
 import com.thatday.base.repository.BaseDao;
 import com.thatday.base.repository.BaseEntity;
-import com.thatday.common.model.PageInfoVo;
-import com.thatday.common.model.PageResult;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
-import javax.validation.constraints.NotNull;
-import java.util.List;
-import java.util.Set;
-import java.util.function.BiConsumer;
+public interface BaseService<ENTITY extends BaseEntity, ID, DAO extends BaseDao<ENTITY, ID>> extends FreeBaseService<ENTITY, ID, DAO> {
 
-public interface BaseService<ENTITY extends BaseEntity, ID, DAO extends BaseDao<ENTITY, ID>> {
-
-    //可自己定义id
-    //自增返回空 id字段需要加上注解@GeneratedValue(strategy = GenerationType.IDENTITY)
-    ID customDatabaseId();
-
-    DAO getDao();
-
-    ENTITY getOne(ID id);
-
-    ENTITY getOneAndCheckNull(ID id, String prefix);
-
-    ENTITY getLastOneById();
-
-    void saveOrUpdate(ENTITY entity);
-
-    <S extends ENTITY> List<S> saveAll(Iterable<S> list);
-
-    /**
-     * 获取全部数据
-     */
-    List<ENTITY> getAll();
-
-    /**
-     * 获取全部数据
-     */
-    List<ENTITY> getAll(SpecificationListener otherConditionListener);
-
-    /**
-     * 获取TARGET全部列表
-     */
-    <TARGET> List<TARGET> getAllToDTO(Class<TARGET> targetClass,
-                                      SpecificationListener otherConditionListener,
-                                      BiConsumer<TARGET, ENTITY> transDTOListener);
-
-    /**
-     * 获取Entity分页列表
-     */
-    Page<ENTITY> getPage(PageRequest pageRequest, SpecificationListener otherConditionListener);
-
-    /**
-     * 获取Entity分页列表
-     */
-    PageResult<ENTITY> getPageResult(PageRequest pageRequest, SpecificationListener otherConditionListener);
-
-    /**
-     * 获取DTO分页列表
-     */
-    <TARGET> PageResult<TARGET> getPageResultToDTO(PageRequest pageRequest, Class<TARGET> targetClass,
-                                                   SpecificationListener otherConditionListener,
-                                                   BiConsumer<TARGET, ENTITY> transDTOListener);
-
-    /**
-     * 获取DTO顶置分页列表
-     */
-    <TARGET> PageResult<TARGET> getStickPageResultToDTO(PageInfoVo vo, @NotNull Set<ID> stickIds, Class<TARGET> targetClass,
-                                                        SpecificationListener otherConditionListener,
-                                                        BiConsumer<TARGET, ENTITY> stickDTOListener,
-                                                        BiConsumer<TARGET, ENTITY> otherDTOListener);
 
 }
--- base-service/src/main/java/com/thatday/base/service/BaseServiceImpl.java
+++ base-service/src/main/java/com/thatday/base/service/BaseServiceImpl.java
@@ -1,58 +1,12 @@
 package com.thatday.base.service;
 
-import com.thatday.base.JPAUtil;
-import com.thatday.base.SpecificationListener;
 import com.thatday.base.repository.BaseDao;
 import com.thatday.base.repository.BaseEntity;
-import com.thatday.common.exception.GlobalException;
-import com.thatday.common.model.PageInfoVo;
-import com.thatday.common.model.PageResult;
-import com.thatday.common.token.UserInfo;
-import com.thatday.common.utils.BeanUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
 
-import javax.validation.constraints.NotNull;
-import java.lang.reflect.Field;
 import java.util.Date;
-import java.util.List;
-import java.util.Set;
-import java.util.function.BiConsumer;
 
-public abstract class BaseServiceImpl<ENTITY extends BaseEntity, ID, DAO extends BaseDao<ENTITY, ID>> implements BaseService<ENTITY, ID, DAO> {
-
-    @Autowired
-    protected DAO dao;
-
-    @Override
-    public DAO getDao() {
-        return dao;
-    }
-
-    @Override
-    public ENTITY getOne(ID id) {
-        return dao.findFirstById(id);
-    }
-
-    @Override
-    public ENTITY getOneAndCheckNull(ID id, String prefix) {
-        ENTITY entity = getOne(id);
-        checkNull(entity, prefix + "错误或不存在");
-        return entity;
-    }
-
-    @Override
-    public ENTITY getLastOneById() {
-        PageRequest pageRequest = JPAUtil.prIdDesc(0, 1);
-        Page<ENTITY> page = getPage(pageRequest, null);
-        if (page.getContent().isEmpty()) {
-            return null;
-        }
-        return page.getContent().get(0);
-    }
+public abstract class BaseServiceImpl<ENTITY extends BaseEntity, ID, DAO extends BaseDao<ENTITY, ID>> extends FreeBaseServiceImpl<ENTITY, ID, DAO> {
 
     @Override
     public void saveOrUpdate(ENTITY entity) {
@@ -61,136 +15,10 @@ public abstract class BaseServiceImpl<ENTITY extends BaseEntity, ID, DAO extends
             setId(customDatabaseId(), entity);
             entity.setCreateTime(new Date());
             entity.setUpdateTime(new Date());
-            dao.save(entity);
+            getDao().save(entity);
         } else {
             entity.setUpdateTime(new Date());
-            dao.saveAndFlush(entity);
-        }
-    }
-
-    @Override
-    public <S extends ENTITY> List<S> saveAll(Iterable<S> list) {
-        return dao.saveAll(list);
-    }
-
-    @Override
-    public List<ENTITY> getAll() {
-        return getAll(null);
-    }
-
-    @Override
-    public List<ENTITY> getAll(SpecificationListener otherConditionListener) {
-        Specification<ENTITY> specification = JPAUtil.makeSpecification((root, criteriaQuery, builder, predicates) -> {
-            if (otherConditionListener != null) {
-                otherConditionListener.addSpecification(root, criteriaQuery, builder, predicates);
-            }
-        });
-        return dao.findAll(specification);
-    }
-
-    @Override
-    public <TARGET> List<TARGET> getAllToDTO(Class<TARGET> targetClass, SpecificationListener otherConditionListener, BiConsumer<TARGET, ENTITY> transDTOListener) {
-        return BeanUtil.transTo(getAll(otherConditionListener), targetClass, transDTOListener);
-    }
-
-    @Override
-    public Page<ENTITY> getPage(PageRequest pageRequest, SpecificationListener otherConditionListener) {
-        Specification<ENTITY> specification = JPAUtil.makeSpecification((root, criteriaQuery, builder, predicates) -> {
-            if (otherConditionListener != null) {
-                otherConditionListener.addSpecification(root, criteriaQuery, builder, predicates);
-            }
-        });
-        return dao.findAll(specification, pageRequest);
-    }
-
-    @Override
-    public PageResult<ENTITY> getPageResult(PageRequest pageRequest, SpecificationListener otherConditionListener) {
-        Page<ENTITY> page = getPage(pageRequest, otherConditionListener);
-        return JPAUtil.setPageResult(pageRequest.getPageNumber(), page);
-    }
-
-    @Override
-    public <TARGET> PageResult<TARGET> getPageResultToDTO(PageRequest pageRequest, Class<TARGET> targetClass,
-                                                          SpecificationListener otherConditionListener,
-                                                          BiConsumer<TARGET, ENTITY> transDTOListener) {
-        Page<ENTITY> pageList = getPage(pageRequest, otherConditionListener);
-        return JPAUtil.setPageResult(pageRequest.getPageNumber(), pageList, targetClass, transDTOListener);
-    }
-
-    @Override
-    public <TARGET> PageResult<TARGET> getStickPageResultToDTO(PageInfoVo vo, @NotNull Set<ID> stickIds, Class<TARGET> targetClass,
-                                                               SpecificationListener otherConditionListener,
-                                                               BiConsumer<TARGET, ENTITY> stickDTOListener,
-                                                               BiConsumer<TARGET, ENTITY> otherDTOListener) {
-        Integer pageSize = vo.getPageSize();
-
-        Page<ENTITY> page = getStickList(vo, stickIds, new JPAUtil.StickPageRequest(vo), false, otherConditionListener);
-        PageResult<TARGET> dtoPageResult = JPAUtil.setPageResult(vo.getCurPage(), page, targetClass, stickDTOListener);
-
-        if (page.getContent().size() < vo.getPageSize()) {
-            vo.setPageSize(vo.getPageSize() - page.getContent().size());
-            Page<ENTITY> otherPage = getStickList(vo, stickIds, new JPAUtil.StickPageRequest(vo), true, otherConditionListener);
-            PageResult<TARGET> otherDto = JPAUtil.setPageResult(vo.getCurPage(), otherPage, targetClass, otherDTOListener);
-
-            dtoPageResult.setTotalCount(dtoPageResult.getTotalCount() + otherDto.getTotalCount());
-            dtoPageResult.getDataList().addAll(otherDto.getDataList());
-            dtoPageResult.setTotalPage((int) (dtoPageResult.getTotalCount() / pageSize) + 1);
-        }
-        return dtoPageResult;
-    }
-
-    private Page<ENTITY> getStickList(PageInfoVo vo, Set<ID> stickIds, JPAUtil.StickPageRequest pageRequest, boolean loadOther, SpecificationListener otherConditionListener) {
-        stickIds.add((ID) "-1");
-
-        if (loadOther) {
-            int stickSize = stickIds.size() - 1;
-            if (vo.getCurPage() >= stickSize / vo.getPageSize() + 1) {
-                pageRequest.setStickOffset(stickSize);
-            }
-        }
-
-        return getPage(pageRequest, (root, criteriaQuery, builder, predicates) -> {
-            if (loadOther) {
-                predicates.add(builder.not(root.get("id").in(stickIds)));
-            } else {
-                predicates.add(builder.and(root.get("id").in(stickIds)));
-            }
-            if (otherConditionListener != null) {
-                otherConditionListener.addSpecification(root, criteriaQuery, builder, predicates);
-            }
-        });
-    }
-
-    protected void checkOwner(UserInfo userInfo, String ownerId) {
-        if (!ownerId.equals(userInfo.getUserId())) {
-            throw GlobalException.createParam("权限不足");
-        }
-    }
-
-    protected void checkNull(Object object, String errorMsg) {
-        if (object == null) {
-            throw GlobalException.createParam(errorMsg);
-        }
-    }
-
-
-    private void setId(ID id, ENTITY entity) {
-        try {
-            Field idField = entity.getClass().getDeclaredField("id");
-            idField.setAccessible(true);
-            idField.set(entity, id);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw GlobalException.createError("no id field");
-        }
-    }
-
-    private ID getId(ENTITY entity) {
-        try {
-            Field idField = entity.getClass().getDeclaredField("id");
-            idField.setAccessible(true);
-            return (ID) idField.get(entity);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw GlobalException.createError("no id field");
+            getDao().saveAndFlush(entity);
         }
     }
 }
