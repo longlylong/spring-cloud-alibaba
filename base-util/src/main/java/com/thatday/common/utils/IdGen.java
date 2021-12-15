7d346ceb2c82bb7fff5c915fe4b3581286f7cff8
King <86416817@qq.com>
2021-12-16 00:20:47 +0800

    id util

--- base-util/src/main/java/com/thatday/common/utils/IdGen.java
+++ base-util/src/main/java/com/thatday/common/utils/IdGen.java
@@ -3,9 +3,7 @@
  */
 package com.thatday.common.utils;
 
-import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.StrUtil;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
@@ -21,8 +19,6 @@ public class IdGen extends IdUtil {
     public static void main(String[] args) {
         System.out.println(TimeId.getNextTimeCode());
         System.out.println(TimeId.getNextTimeCode());
-        System.out.println(TimeId.getNextTimeCode(17200));
-        System.out.println(TimeId.getNextTimeCode(7200));
         System.out.println(IdUtil.objectId());
         System.out.println(IdUtil.objectId());
         System.out.println(IdUtil.getSnowflake(1, 2).nextIdStr());
@@ -32,8 +28,8 @@ public class IdGen extends IdUtil {
     private static final SecureRandom random = new SecureRandom();
 
     //时间序列id 多个服务的话用这个 port为每个服务的端口号
-    public static String getNextTimeCode(int port) {
-        return TimeId.getNextTimeCode(port);
+    public static String getNextTimeCode(int workerId, int datacenterId) {
+        return TimeId.getNextTimeCode(workerId, datacenterId);
     }
 
     //时间序列id  单体服务的话用这个
@@ -106,45 +102,14 @@ public class IdGen extends IdUtil {
 
     private final static class TimeId {
 
-        private static volatile int num = 100000;
-        private static final long startTime = DateUtil.parseDate("2020-07-01").getTime();
-
-        //多个服务的话用这个 port为每个服务的端口号
-        public static String getNextTimeCode(int port) {
-            //(4位 + 6位) + 5位 + 6位
-            return getNextTime() + String.format("%05d", port) + nextTimeInt();
+        //多个服务的话用这个
+        public static String getNextTimeCode(int workerId, int datacenterId) {
+            return IdUtil.getSnowflake(workerId, datacenterId).nextIdStr();
         }
 
         //单体服务的话用这个
         public static String getNextTimeCode() {
-            //(4位 + 6位) + 6位
-            return getNextTime() + nextTimeInt();
-        }
-
-        //根据时间生成的数据库id
-        private static String getNextTime() {
-            long seconds = (System.currentTimeMillis() - startTime) / 1000;
-            long day = seconds / (86400);
-            long m = seconds % (86400);
-            //4位 + 6位
-            return String.format("%04d", day) + fixedLength(m);
-        }
-
-        private static String fixedLength(long num) {
-            return StrUtil.fill(num + "", '0', 6, false);
-        }
-
-        private static synchronized int nextTimeInt() {
-            //还可以判断不同的类序列自增
-            //还可以判断同一秒是否到了999999
-
-            if (num >= 999999) {
-                num = 100000;
-            } else {
-                int i = random.nextInt(4) + 1;
-                num = num + i;
-            }
-            return num;
+            return IdUtil.getSnowflake(1, 1).nextIdStr();
         }
     }
 }
